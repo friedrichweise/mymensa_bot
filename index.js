9c6b522238e62a0ebe0a1b8725d8518f4b982a40
friedrichweise <hi@weise.io>
2017-07-02 21:15:31 +0200

    code cleanup part I: use const and let; nicer callback structure

--- index.js
+++ index.js
@@ -1,139 +1,157 @@
-var token = require('./token.js').token;
-var mensa = require('./openmensa.js');
-var Bot = require('node-telegram-bot-api'),
+const token = require('./token.js').token;
+const mensa = require('./openmensa.js');
+const Bot = require('node-telegram-bot-api'),
     bot = new Bot(token, { polling: true });
 
-
-var extras = {"items":[
+const extras = {"items":[
           {"icon": "🌱","keywords": ["vegetarisch","vegan"]},
           {"icon": "🍷", "keywords":["Alkohol"]},
           {"icon": "🐮", "keywords":["Rindfleisch"]},
           {"icon": "🐷", "keywords":["Schweinefleisch"]}
 ]};
 
-var helpText = `
+const helpText = `
 Welcome to mymensa_bot \n
 Please use the /mensa command to search for canteens in your city (example: /mensa Berlin). 
 You are going to receive a custom keyboard that contains further commands.`;
 
-/////////////////////
-//get canteen by city
-/////////////////////
-
+/*
+ * Requests of type /mensa city
+ * Call the OpenMensa API and answers to the message
+ */
 bot.onText(/^\/mensa (.+)$/, function(msg, match){
-  var city = match[1];
-  bot.sendChatAction(msg.chat.id, 'typing').then(function () {
-      mensa.getCanteenByCity(msg, city, responseCanteenCity);
+  const city = match[1];
+  bot.sendChatAction(msg.chat.id, 'typing').then(() => {
+      mensa.getCanteenByCity(city, (result) => {
+        const text = `Ergebnisse für ${city}:`;
+        const opts = createCanteenKeyboard(result, city);
+        if(opts) bot.sendMessage(msg.chat.id, 'Ergebnisse für '+city+':', opts);
+        else botSendError(msg);
+      });
   });
 });
 
-function responseCanteenCity(msg, result, city) {
-  if(result===null) {
-    botSendError(msg);
+/*
+ * creates telegram custom keyboard from all canteens in one city
+ */
+function createCanteenKeyboard(canteens, city) {
+  if(!canteens) return null;
+  let out = [];
+  for (i=0; i<canteens.length; i++) {
+    const re = new RegExp(city+',', 'gi');
+    const name = canteens[i].name.replace(re, '');
+    const keyboard_object = {text:name, callback_data: `${canteens[i].id}`};
+    out.push([keyboard_object]);
   }
-  else {
-    var out = [];
-    for (i=0; i<result.length; i++) {
-        var re = new RegExp(city+',', 'gi');
-        var name = result[i].name.replace(re, '');
-        var keyboard_object = {text:name, callback_data: `${result[i].id}`};
-        out.push([keyboard_object]);
-    }
-    if(out.length==0) botSendError(msg);
-    else {
-      console.log(out);
-      var opts = {
-          reply_to_message_id: msg.message_id,
-          reply_markup: JSON.stringify({inline_keyboard: out})
-      };
-      bot.sendMessage(msg.chat.id, 'Ergebnisse für '+city+':', opts);
-    }
-  }
+  if(out.length===0) return null;
+  const opts = {
+      reply_markup: JSON.stringify({inline_keyboard: out})
+  };
+  return opts;
 }
 
-// Handle callback queries
+/*
+ * gets called after the user clicked on a custom keyboard key
+ * return the full meal information for the mensa
+ */
 bot.on('callback_query', function onCallbackQuery(callbackQuery) {
-  const action = callbackQuery.data;
+  const canteenID = callbackQuery.data;
   const msg = callbackQuery.message;
-
+  const opts = {
+    chat_id: msg.chat.id,
+    message_id: msg.message_id,
+    parse_mode: 'Markdown'
+  };
+  //Das will man eigentlich
   bot.sendChatAction(msg.chat.id, 'typing').then(function () {
-      mensa.getMealsByID(msg, action, '', responseMeals);
+    mensa.getMealsByID(canteenID, (result, canteenName) => {
+      if(!result) botSendError(msg);
+      const text = createMealResponse(result, canteenName, '');
+      if(!text) botSendError(msg)
+      else bot.editMessageText(text, opts);
+    })
   });
+
 });
 
-/////////////////////////
-//get meals by canteen id
-/////////////////////////
+/*
+ * Requests of type /meals id
+ * call the OpenMensa and returns meals
+ */
 bot.onText(/^\/meals (.+)$/, function(msg, match){
-  var chatID = msg.chat.id;
-  console.log(msg);
-
-  var input = match[1].split(' ');
-  var canteenID = input[0];
-  var extraModifier = '';
-  for(var a=0; (input.length>1 && a<extras.items.length); a++) {
+  const chatID = msg.chat.id;
+  const opts = {
+        parse_mode: 'Markdown',
+  };
+  const input = match[1].split(' ');
+  const canteenID = input[0];
+  let extraModifier = '';
+  for(a=0; (input.length>1 && a<extras.items.length); a++) {
       if(input[1]==extras.items[a].icon) extraModifier = extras.items[a].icon;
   }
-
   bot.sendChatAction(chatID, 'typing').then(function () {
-      mensa.getMealsByID(msg, canteenID, extraModifier, responseMeals);
+    mensa.getMealsByID(canteenID, (result, canteenName) => {
+      if(!result) botSendError(msg);
+      const text = createMealResponse(result, canteenName, extraModifier);
+      if(!text) botSendError(msg)
+      else bot.sendMessage(chatID, text, opts);
+    })
   });
 });
 
-function responseMeals(msg, result, extraResult) {
-    var output = '';
-    if(result===null) {
-      botSendError(msg);
-      return;
-    }
-    if(extraResult.canteenName!=null) output+='Ergebnisse für '+extraResult.canteenName+':\n\n';
-    for (i=0; i<result.length; i++) {
-      var meal = result[i];
-      var flag = false;
-      var notes = '';
-      //add extra icons
-      for(var a=0; a<extras.items.length; a++) {
-        var extra = extras.items[a];
-        if(evaluateNotes(meal.notes,extra.keywords)) {
-          notes+=extra.icon;
-          if(extraResult.modifier==extra.icon) flag = true;
-        }
-      }
-      if(flag==true || extraResult.modifier=='') {
-        output+='`▸ '+meal.category+'` : *'+meal.name+'*';
-        if(meal.prices.students!=null) output+= ', Student: *'+formatPrice(meal.prices.students)+'*€';
-        if(meal.prices.employees!=null) output+= ' Mitarbeiter: *'+formatPrice(meal.prices.employees)+'*€';
-        output+=' '+notes;
-        output+='\n';
+/*
+ * create human readable string of all meals offered by a single cantenn
+ */
+function createMealResponse(meals, canteen, modifiers) {
+  let output = '';
+  if(!meals) return null;
+  if(canteen) output = `Ergebnisse für ${canteen} \n\n`;
+  for (i=0; i<meals.length; i++) {
+    const meal = meals[i];
+    const flag = false;
+    let notes = '';
+    //add extra icons
+    for(a=0; a<extras.items.length; a++) {
+      const extra = extras.items[a];
+      if(evaluateNotes(meal.notes,extra.keywords)) {
+        notes+=extra.icon;
+        if(modifiers===extra.icon) flag = true;
       }
     }
-    if(output=='') {
-      botSendError(msg);
-      return;
-    }
-    var opts = {
-          parse_mode: 'Markdown',
-    };
-    bot.sendMessage(msg.chat.id, output, opts);
+    if(flag==true || modifiers==='') {
+      output+='`▸ '+meal.category+'` : *'+meal.name+'*';
+      if(meal.prices.students!=null) output+= ', Student: *'+formatPrice(meal.prices.students)+'*€';
+      if(meal.prices.employees!=null) output+= ' Mitarbeiter: *'+formatPrice(meal.prices.employees)+'*€';
+      output+=' '+notes;
+      output+='\n';
+    } 
+  }
+  return output;
 }
+
+/*
+ * Helper functions
+ */
 function evaluateNotes(notes,query) {
   if(notes===null | query===null) return false;
-  for(var i=0; i<notes.length; i++) {
-    for(var a=0; a<query.length; a++) {
+  for(i=0; i<notes.length; i++) {
+    for(a=0; a<query.length; a++) {
         if(notes[i].indexOf(query[a])>-1) return true;
     }
   }
   return false;
 }
 function formatPrice(price) {
-  var parts = price.toString().split('.');
+  let parts = price.toString().split('.');
   if(parts!=null && parts.length==2) {
     if(parts[1].length==1) return parts[0]+"."+parts[1]+"0";
   }
   return price;
 }
 
-
+/*
+ * return help text
+ */
 bot.onText(/\/help/,function(msg){
   bot.sendMessage(msg.chat.id, helpText);
 });
@@ -141,9 +159,9 @@ bot.onText(/\/start/,function(msg){
   bot.sendMessage(msg.chat.id, helpText);
 });
 
-///////////////////
-//global error call
-///////////////////
+/*
+ * default error call
+ */
 function botSendError(msg) {
   bot.sendMessage(msg.chat.id, '❌ Keine Ergebnisse');
 }
--- openmensa.js
+++ openmensa.js
@@ -1,41 +1,39 @@
-var http = require('http');
-var canteens;
+const http = require('http');
+let canteens;
 
-exports.getCanteenByCity = function (msg, city, callback) { 
-	var result = [];
-	var path = '/api/v2/canteens?limit=500';
+exports.getCanteenByCity = function (city, callback) { 
+	let result = [];
+	const path = '/api/v2/canteens?limit=500';
 	mensaCall(path, function(response){
 		//simple canteen cache
 		canteens = response;
 		//filter by city
-		if(response===null) return callback(msg, response)
+		if(response===null) return callback(null)
 		for(i=0; i<response.length; i++) {
-			var mensa = response[i];
-			if(mensa.city.toLowerCase()==city.toLowerCase()) result.push(mensa);
+			let mensa = response[i];
+			if(mensa.city.toLowerCase() === city.toLowerCase()) result.push(mensa);
 		}
-		callback(msg, result, city);	
+		callback(result);	
 	});
 }
 
-exports.getMealsByID = function(msg, canteenID,extraModifier, callback) {
-	var path = '/api/v2/canteens/'+canteenID+'/days/'+getDateString()+'/meals';
+exports.getMealsByID = function(canteenID, callback) {
+	const path = '/api/v2/canteens/'+canteenID+'/days/'+getDateString()+'/meals';
 	mensaCall(path,function(response){
 		//get Canteen Name
-		var canteenName = null;
+		let canteenName = null;
 		if(canteens!=null){
 			for(i=0; i<canteens.length; i++) {
 				if(canteens[i].id==canteenID) canteenName=canteens[i].name;
 			}
 		}
-		callback(msg, response, {"modifier":extraModifier,"canteenName":canteenName});
+		callback(response, canteenName);
 	});
 }
 
 function getDateString() {
-	var today = new Date();
+	const today = new Date();
 	return (today.getMonth()+1)+'-'+(today.getDate())+'-'+today.getFullYear();
-	//@testing
-	//return "12-8-2015";
 }
 
 function mensaCall(path, callback) {
@@ -43,12 +41,12 @@ function mensaCall(path, callback) {
         host: 'openmensa.org',
         path: path
     }, function(response) {
-        var body = '';
+        let body = '';
         response.on('data', function(d) {
             body += d;
         });
         response.on('end', function() {
-        	var result;
+        	let result;
         	try {
         		result = JSON.parse(body);
     		} catch (e) {
